ad3823955562e003fe750e5572181a08cfb39985
David Farias <dfarias@redhat.com>
2022-07-06 13:15:43 -0300

    maked some adjustes

--- extensions-bkp/gen_configmap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a configmap
-oc create configmap cli-script --from-file=postconfigure.sh=extensions/postconfigure.sh --from-file=my-cli-script.cli=extensions/my-cli-script.cli
-
-# Mounting the volume with the configmap
-oc set volumes dc/my-eap --add --name=cli-script --mount-path=/opt/eap/extensions --type=configmap --configmap-name=cli-script --default-mode='0755' --overwrite
--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -1,3 +1,3 @@
 echo "Executing script" >> /tmp/test
-${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/extensions/cli-script.cli
+${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/extensions/actions.cli
 echo "Finishing script execution" >> /tmp/test
