b99e78451423415497c80d2e5256af131da856de
David Farias <dfarias@redhat.com>
2022-07-07 15:49:51 -0300

    some adjustments made

--- extensions/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -x
-injected_dir=$1
-cp -rf ${injected_dir} $JBOSS_HOME/extensions
--- extensions/postconfigure.sh
+++
@@ -1,3 +0,0 @@
-echo "Executing script" >> /tmp/test
-${JBOSS_HOME}/bin/jboss-cli.sh --file=${JBOSS_HOME}/extensions/actions.cli
-echo "Finishing script execution" >> /tmp/test
